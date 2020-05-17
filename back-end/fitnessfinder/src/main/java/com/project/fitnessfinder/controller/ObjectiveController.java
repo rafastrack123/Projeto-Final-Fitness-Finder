0941eb924863f51fe843e1a65c9c3d619a36871c
Rafael Strack <rafaelstrack@Rafaels-MacBook-Pro.local>
2020-05-17 19:51:30 -0300

    allowed cors

--- back-end/fitnessfinder/src/main/java/com/project/fitnessfinder/controller/ObjectiveController.java
+++ back-end/fitnessfinder/src/main/java/com/project/fitnessfinder/controller/ObjectiveController.java
@@ -4,6 +4,7 @@ import com.project.fitnessfinder.domain.entity.api.ObjectiveJson;
 import com.project.fitnessfinder.service.ObjectiveService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +17,9 @@ public class ObjectiveController {
     private final ObjectiveService objectiveService;
 
     @GetMapping("/findAll")
-    public List<ObjectiveJson> findAll(){
+    public List<ObjectiveJson> findAll() {
 
-         var x = objectiveService.findAll();
-
-         return x;
+        return objectiveService.findAll();
     }
 
 
