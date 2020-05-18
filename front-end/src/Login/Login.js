3fab579555904522780824a2badb4f6802286f93
Rafael Strack <rafaelstrack@Rafaels-MacBook-Pro.local>
2020-05-18 20:52:10 -0300

    sign up to server

--- front-end/src/Login/Login.js
+++ front-end/src/Login/Login.js
@@ -1,38 +1,76 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Login.css';
+import axios from 'axios';
 
-const login = () => {
-    return (
-        <form>
-        <h3>Login</h3>
+class Login extends Component {
 
-        <div className="form-group">
-            <label>Email</label>
-            <input type="email" className="form-control" placeholder="Insira email" />
-        </div>
+    state = {
+        login: "",
+        password: ""
+    }
 
-        <div className="form-group">
-            <label>Senha</label>
-            <input type="password" className="form-control" placeholder="Insira senha" />
-        </div>
+    loginChangeHandle = (event) => {
+        this.setState({ login: event.target.value });
+    }
 
-        {/* <div className="form-group">
+    passwordChangeHandle = (event) => {
+        this.setState({ password: event.target.value });
+    }
+
+    authenticate = () => {
+
+        var userLogin = {
+            username: this.state.login,
+            password: this.state.password
+        }
+
+        axios.post('http://localhost:8080/login', userLogin)
+            .then(response => console.log(response.data))
+            .catch(response => console.log("ERRO!"));
+    }
+
+    render() {
+        return (
+            <form>
+                <h3>Login</h3>
+
+                <div className="form-group">
+                    <label>Email</label>
+
+                    <input type="text"
+                        className="form-control"
+                        value={this.state.login}
+                        onChange={this.loginChangeHandle}
+                        placeholder="Insira email" />
+                </div>
+
+                <div className="form-group">
+                    <label>Senha</label>
+                    <input type="password"
+                        className="form-control"
+                        value={this.state.password}
+                        onChange={this.passwordChangeHandle}
+                        placeholder="Insira senha" />
+                </div>
+
+                {/* <div className="form-group">
             <div className="custom-control custom-checkbox">
                 <input type="checkbox" className="custom-control-input" id="customCheck1" />
                 <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
             </div>
         </div> */}
 
-        <button type="submit" className="btn btn-primary btn-block">Entrar</button>
-        {/* <p className="forgot-password text-right">
+                <button type="button" className="btn btn-primary btn-block" onClick={this.authenticate}>Entrar</button>
+                {/* <p className="forgot-password text-right">
             Forgot <a href="#">password?</a>
         </p> */}
-        <p className="forgot-password text-right">
-                Ainda não tem cadastro? <a href="/sign-up">Inscrever-se</a>
-        </p>
-    </form>
-    )
+                <p className="forgot-password text-right">
+                    Ainda não tem cadastro? <a href="/sign-up">Inscrever-se</a>
+                </p>
+            </form>
+        )
+    }
 }
 
 
-export default login;
+export default Login;
