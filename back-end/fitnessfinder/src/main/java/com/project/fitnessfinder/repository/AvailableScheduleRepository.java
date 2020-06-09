c7b31feeba7e0411da7a403e3d3154269352a1bc
Rafael Strack <rafaelstrack@Rafaels-MacBook-Pro.local>
2020-06-09 20:18:09 -0300

    vendor detail endpoits

--- back-end/fitnessfinder/src/main/java/com/project/fitnessfinder/repository/AvailableScheduleRepository.java
+++ back-end/fitnessfinder/src/main/java/com/project/fitnessfinder/repository/AvailableScheduleRepository.java
@@ -1,7 +1,15 @@
 package com.project.fitnessfinder.repository;
 
 import com.project.fitnessfinder.domain.entity.database.AvailableSchedule;
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface AvailableScheduleRepository extends CrudRepository<AvailableSchedule, Long> {
+
+    @Query("select availableSchedule" +
+            " from AvailableSchedule as availableSchedule" +
+            " join availableSchedule.vendorOffer as vendorOffer " +
+            " where vendorOffer.id=?1 ")
+    List<AvailableSchedule> findByVendorOfferId(Long vendorOfferId);
 }
